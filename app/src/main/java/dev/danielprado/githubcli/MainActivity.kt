d989bad6e2c5872da6a50f6288a68e262404a109
Daniel Prado <dfe.prado@gmail.com>
2021-06-29 22:38:27 -0300

    Refatoração do fluxo de login do GitHub: separação do autenticador do GitHub em uma classe e criação de classes auxiliares

--- app/src/main/java/dev/danielprado/githubcli/MainActivity.kt
+++ app/src/main/java/dev/danielprado/githubcli/MainActivity.kt
@@ -3,21 +3,17 @@ package dev.danielprado.githubcli
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import dev.danielprado.githubcli.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import java.io.*
-import java.net.HttpURLConnection
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
-
-private const val GH_URL_CODE_EXCHANGE = "https://github.com/login/oauth/access_token"
-private const val GH_CLI_SECRET = "065ad7b372c88a4898a0a175e428cabb9033d715"
 
 @Serializable
 data class GithubLoginCodeExchangePost(
@@ -33,59 +29,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         layoutBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(layoutBinding.root)
-
-        layoutBinding.btnLogIn.setOnClickListener {
-            GithubAuthDialog() { processCode(it) }.show(supportFragmentManager, null)
-        }
     }
 
-    private fun processCode(code: String) {
-        Log.i("GITHUB_LOGIN", "Your code is $code")
+    fun login(view: View) {
+        GithubAuthenticator(
+            "c3daa1d5d1d813ca449d",
+            "065ad7b372c88a4898a0a175e428cabb9033d715",
+            "MyTestApplication",
+            ::onLoginSuccess,
+            ::onLoginFail
+        ).doLogin(supportFragmentManager)
+    }
 
-        // prepare the JSON for code exchange
-        val codeExchangePost = GithubLoginCodeExchangePost(
-                GH_CLI_ID,
-                GH_CLI_SECRET,
-                code
-        );
+    private fun onLoginSuccess(token: Token) {
+        Log.i("MAIN_LOGIN", token.value)
+    }
 
-        // prepare URL
-        val url = URL(GH_URL_CODE_EXCHANGE)
-//        HttpURLConnection
-        val post = url.openConnection() as HttpsURLConnection
-        post.apply {
-            requestMethod = "POST"
-            setRequestProperty("Content-Type", "application/json")
-            setRequestProperty("Accept", "application/json")
-            doOutput = true
-        }
-        CoroutineScope(Dispatchers.IO).launch {
-            val postJson = Json.encodeToString<GithubLoginCodeExchangePost>(codeExchangePost)
-            val out = DataOutputStream(post.outputStream)
-            out.write(postJson.toByteArray())
-            out.flush()
-            out.close()
-
-
-            if (post.responseCode == 200) {
-                var response: String = ""
-                try {
-                    val input = BufferedReader(InputStreamReader(post.inputStream))
-                    val content = StringBuffer()
-                    var inputLine = input.readLine()
-                    while (inputLine != null) {
-                        content.append(inputLine)
-                        inputLine = input.readLine()
-                    }
-                    input.close()
-                    response = content.toString()
-                } finally {
-                    post.disconnect()
-                }
-                Log.i("GITHUB_LOG", response)
-            } else {
-                Log.i("GITHUB_LOG", "Failed to exchange code")
-            }
-        }
+    private fun onLoginFail(reason: String) {
+        Log.i("MAIN_LOGIN", reason)
     }
 }
