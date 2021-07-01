1c01679290794b46457ccf31915eb4acb953782e
Daniel Prado <dfe.prado@gmail.com>
2021-06-30 21:44:40 -0300

    Separa o diálogo de autenticação do GitHub numa lib; implementa a lib AndroidAuthenticator (dfeprado/android_authenticator.git)

--- app/src/main/java/dev/danielprado/githubcli/MainActivity.kt
+++ app/src/main/java/dev/danielprado/githubcli/MainActivity.kt
@@ -4,23 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import dev.danielprado.androidauthenticatorsapp.GH_APP_ID
+import dev.danielprado.androidauthenticatorsapp.GH_APP_SECRET
 import dev.danielprado.githubcli.databinding.ActivityMainBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-import java.io.*
-import java.net.URL
-import javax.net.ssl.HttpsURLConnection
-
-@Serializable
-data class GithubLoginCodeExchangePost(
-    val client_id: String,
-    val client_secret: String,
-    val code: String
-);
 
 class MainActivity : AppCompatActivity() {
     lateinit private var layoutBinding: ActivityMainBinding
@@ -32,16 +18,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun login(view: View) {
-        GithubAuthenticator(
-            "c3daa1d5d1d813ca449d",
-            "065ad7b372c88a4898a0a175e428cabb9033d715",
-            "MyTestApplication",
-            ::onLoginSuccess,
-            ::onLoginFail
-        ).doLogin(supportFragmentManager)
+        GithubAuthenticatorDialog(
+                "MyTestApplication",
+                GH_APP_ID,
+                GH_APP_SECRET,
+                ::onLoginSuccess,
+                ::onLoginFail
+        ).show(supportFragmentManager, null)
     }
 
-    private fun onLoginSuccess(token: Token) {
+    private fun onLoginSuccess(token: GithubAuthToken) {
         Log.i("MAIN_LOGIN", token.value)
     }
 
